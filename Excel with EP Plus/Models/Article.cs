4b524afaff4a9f9cc5d53d94bfd4f98056fc4bf4
Kunlapath <kunlapath.paengsa@gmail.com>
2022-10-26 15:23:38 +0700

    Add project files.

---
+++ Excel with EP Plus/Models/Article.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_with_EP_Plus.Models
+{
+    public class Article
+    {
+        //public Article()
+        //{
+        //    new{ Id = "101", Name = "C++" },
+        //    new
+        //    {
+        //        Id = "102",
+        //        Name = "Python"
+        //    },
+        //        new
+        //        {
+        //            Id = "103",
+        //            Name = "Java Script"
+        //        },
+        //        new
+        //        {
+        //            Id = "104",
+        //            Name = "GO"
+        //        },
+        //        new
+        //        {
+        //            Id = "105",
+        //            Name = "Java"
+        //        },
+        //        new
+        //        {
+        //            Id = "106",
+        //            Name = "C#"
+        //        }
+        //}}
+        public class Info
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+
+
+        }
+    }
+}
